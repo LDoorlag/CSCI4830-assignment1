d4eb0dbff2d9cc784f6accf732b2bdf2177b37f9
LDoorlag <ljdoorlag@icloud.com>
2025-09-30 14:45:42 -0500

    first commit for assignment 1

---
+++ HelloWorld.cpp
@@ -0,0 +1,16 @@
+// Simple C++ program to display "Hello World" 
+//Author: Logan Doorlag <ldoorlag@unomaha.edu>
+// Header file for input output functions 
+#include<iostream>  
+  
+using namespace std; 
+  
+// main function - 
+// where the execution of program begins 
+int main() 
+{ 
+    // prints hello world 
+    cout<<"Hello World"; 
+      
+    return 0; 
+} 
